daa01e64a07f618eb91bc86c9cfac4853e1bc6ff
yudinurkurniawan <yudi@solusimatika.com>
2017-08-25 15:06:36 +0700

    nambahin comment

--- index.php
+++ index.php
@@ -63,6 +63,9 @@
  * Different environments will require different levels of error reporting.
  * By default development will show errors but testing and live will hide them.
  */
+ 
+ 
+ //nambahin comment
 switch (ENVIRONMENT)
 {
 	case 'development':
